75d1690bf8e97014e66414f6d5f8eef9a8c059c5
Alex Pimenta <aspepper@hotmail.com>
2024-02-25 16:01:48 -0300

    first commit

---
+++ CSharp-Simple-Sample/Program.cs
@@ -0,0 +1,39 @@
+﻿class BankAccount
+{
+    private double balance;
+
+    public void Deposit(double value)
+    {
+        balance += value;
+    }
+
+    public void Withdraw(double value)
+    {
+        if (value <= balance)
+        {
+            balance -= value;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds");
+        }
+    }
+
+    public double CheckBalance()
+    {
+        return balance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new();
+        Console.WriteLine($"Current balance: {account.CheckBalance()}");
+        account.Deposit(1000);
+        Console.WriteLine($"Current balance: {account.CheckBalance()}");
+        account.Withdraw(500);
+        Console.WriteLine($"Current balance: {account.CheckBalance()}");
+    }
+}
